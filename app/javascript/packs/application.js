adcafcffd8e566ff001a48db8f587acb5c18bc93
Eduardo Veras <eduardo.storm@gmail.com>
2020-10-18 17:27:56 -0400

    Addinng materialize

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import "../stylesheets/application"
+import 'materialize-css/dist/js/materialize'
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
