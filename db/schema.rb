475675a04ac3f28b0cabf4721f6debb6a1f0c6d4
Eduardo Veras <eduardo.storm@gmail.com>
2020-10-25 10:59:15 -0400

    adding base calendar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_162314) do
+ActiveRecord::Schema.define(version: 2020_10_25_141255) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
